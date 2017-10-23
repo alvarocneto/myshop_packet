9f26f8040b2bf3f29acbcccd2d5002be1f50d3a5
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-10-23 18:23:15 -0200

    Criação de carrinho

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from .models import Category, Product
 
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
-
-
 admin.site.register(Category, CategoryAdmin)
 
 
@@ -15,6 +12,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated', 'category']
     list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
-
-
 admin.site.register(Product, ProductAdmin)
